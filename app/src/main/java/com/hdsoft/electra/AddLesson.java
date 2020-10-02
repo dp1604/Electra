3f7a4d64e71a960689289110a8fa95f24b57b9ed
Jayanath98 <bhasurajayanath@gmail.com>
2020-10-03 00:08:16 +0530

    New UI Added

---
+++ app/src/main/java/com/hdsoft/electra/AddLesson.java
@@ -0,0 +1,14 @@
+package com.hdsoft.electra;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddLesson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_lesson);
+    }
+}
