eeca6826ce3378ab32b3411419f4e1ce4f660beb
Jayanath98 <bhasurajayanath@gmail.com>
2020-10-03 18:22:12 +0530

    Page transition created

--- app/src/main/java/com/hdsoft/electra/MainActivity.java
+++ app/src/main/java/com/hdsoft/electra/MainActivity.java
@@ -3,12 +3,48 @@ package com.hdsoft.electra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddLesson.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ReadLesson.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ReadLesson.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/hdsoft/electra/ReadLesson.java
+++ app/src/main/java/com/hdsoft/electra/ReadLesson.java
@@ -1,14 +1,39 @@
 package com.hdsoft.electra;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReadLesson extends AppCompatActivity{
 
+    public Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.read_lesson);
+
+        button = (Button) findViewById(R.id.button5);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReadLesson.this,UpdateLesson.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReadLesson.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
