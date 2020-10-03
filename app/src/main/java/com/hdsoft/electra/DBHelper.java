952a27e95dd632faab49a1c6b3b590dd4edf9e5c
thimira1 <thimira97.3.28@gmail.com>
2020-10-03 13:15:21 +0530

    Add My Part

---
+++ app/src/main/java/com/hdsoft/electra/DBHelper.java
@@ -0,0 +1,82 @@
+package com.hdsoft.electra;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "Userdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Userdetails (comment TEXT primary key)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists Userdetails");
+    }
+
+    public Boolean insertuserdata(String comment) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("comment",comment);
+        long result = DB.insert("Userdetails", null,contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public Boolean updateuserdata(String comment) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where comment = ?",new String[] {comment});
+        if (cursor.getCount()>0)
+        {
+            long result = DB.update("Userdetails", contentValues, "comment=?", new String[]{comment});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }else
+            {
+                return false;
+        }
+    }
+
+    public Boolean deletedata(String comment) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where comment = ?",new String[] {comment});
+        if (cursor.getCount()>0)
+        {
+            long result = DB.delete("Userdetails",  "comment=?", new String[]{comment});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }else
+        {
+            return false;
+        }
+    }
+
+    public Cursor getdata() {
+        SQLiteDatabase DB = this.getWritableDatabase();
+
+        Cursor cursor = DB.rawQuery("Select * from Userdetails",null);
+        return cursor;
+    }
+
+
+}
--- app/src/main/java/com/hdsoft/electra/MainActivity.java
+++ app/src/main/java/com/hdsoft/electra/MainActivity.java
@@ -1,16 +1,24 @@
 package com.hdsoft.electra;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
-import android.content.Intent;
-import android.view.View;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-   public Button button;
+
+    TextView comment;
+    Button insert, update, delete, view;
+    DBHelper DB;
+
 
 
     @Override
@@ -18,18 +26,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.btnAdd);
+        comment = findViewById(R.id.textView);
 
+        insert = findViewById(R.id.btnAdd);
+        update = findViewById(R.id.btnUPDATE);
+        delete = findViewById(R.id.btnDELETE);
+        view = findViewById(R.id.btnVIEW);
+        DB = new DBHelper(this);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        //Insert Data
+        insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CommentView.class);
-                startActivity(intent);
+                String commentTXT = comment.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(commentTXT);
+                if (checkinsertdata == true)
+                    Toast.makeText(MainActivity.this, "Comment Added Successfully", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Comment Not Inserted", Toast.LENGTH_SHORT).show();
             }
         });
 
+       //Update Data
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String commentTXT = comment.getText().toString();
 
+                Boolean checkupdatetdata = DB.updateuserdata(commentTXT);
+                if (checkupdatetdata == true)
+                    Toast.makeText(MainActivity.this, "Comment Updated Successfully", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Comment Not Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+       //Delete Data
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String commentTXT = comment.getText().toString();
+
+                Boolean checkdeletetdata = DB.deletedata(commentTXT);
+                if (checkdeletetdata == true)
+                    Toast.makeText(MainActivity.this, "Comment Deleted Successfully", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Comment Not Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+       //View Data
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res =DB.getdata();
+                if (res.getCount() == 0) {
+                    Toast.makeText(MainActivity.this, "No Comments to View", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()) {
+                    buffer.append("Comment :"+res.getString( 0));
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Your Comment");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }
+        });
     }
 
 
