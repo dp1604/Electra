ad2108a1d959909d20190d0e5ff83db4a5b9b10c
Jayanath98 <bhasurajayanath@gmail.com>
2020-10-04 15:59:27 +0530

    Again updated ui

---
+++ app/src/main/java/com/hdsoft/electra/LessonModelClass.java
@@ -0,0 +1,14 @@
+package com.hdsoft.electra;
+
+public class LessonModelClass {
+
+    String topic;
+    String things;
+    String begin;
+
+    public LessonModelClass(String topic, String things, String begin) {
+        this.topic = topic;
+        this.things = things;
+        this.begin = begin;
+    }
+}
--- app/src/main/java/com/hdsoft/electra/MainActivity.java
+++ app/src/main/java/com/hdsoft/electra/MainActivity.java
@@ -6,33 +6,36 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button;
+    EditText editText_topic,editText_things,editText_begin;
+    Button button_add,button_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button3);
+        editText_topic = findViewById(R.id.editText_topic);
+        editText_things = findViewById(R.id.editText_things);
+        editText_begin = findViewById(R.id.editText_begin);
+        button_add = findViewById(R.id.button_add);
+        button_view = findViewById(R.id.button_view);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+                String stringTopic = editText_topic.getText().toString();
+                String stringThings = editText_things.getText().toString();
+                String stringBegin = editText_begin.getText().toString();
 
-        button = (Button) findViewById(R.id.button2);
+                if (stringTopic.length() <=0 || stringThings.length() <=0 || stringBegin.length() <=0){
+                    Toast.makeText(MainActivity.this, "You should enter all data", Toast.LENGTH_SHORT).show();
+                }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ReadLesson.class);
-                startActivity(intent);
             }
         });
     }
